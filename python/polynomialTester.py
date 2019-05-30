357a1f4c52ebe21c4c84979b814e89373f1dd0e4
keaun wild <48817136+Keanu91@users.noreply.github.com>
2019-05-30 22:26:09 +1000

    Script Finished Testing required

--- python/polynomialTester.py
+++ python/polynomialTester.py
@@ -8,17 +8,13 @@ apower = []
 answer = []
 sumOfAll = []
 
-
-acoef = [1,-4,-12]
-apower = [2,1,0]
-
-def sec():
+def inputUser(): # taking in input of user
     coef = int()
     power = int()
     print("Enter Coefficient " + str(i))
     coef = int(input())
     print("Enter Power " + str(i))
-    power = int(input())
+    power = int(input(str(coef)+"x^"))
     return(coef,power)
 
 def division(coef,x,power):
@@ -26,37 +22,30 @@ def division(coef,x,power):
     res = float(coef*math.pow(x,power))
     return(res)
 
-def counter():
-    for i2 in range(-10,10):
+def counter(): #calculating each section
+    for i2 in range(-100,100): #ramge of x variables tested
         global x
         x = i2
         count = [0]
-        count.append(division(acoef[0],x,apower[0]))
-        print(count)
-        count.append(division(acoef[1],x,apower[1]))
-        print(count)
-        count.append(division(acoef[2],x,apower[2]))
-        print(count)
+        for i3 in range(0,order+1):
+            count.append(division(acoef[i3],x,apower[i3]))
+            print(count)
         sumOfAll.append(sum(count))
         if sumOfAll[-1] != 0:
             sumOfAll.pop()
         else:
             answer.append(x)
-    return("Success")
 
-#for i in range(0,order+1):
-#    var = sec()
-#    var1 = var[0]
-#    var2 = var[1]
-#    acoef.append(var1)
-#    apower.append(var2)
+for i in range(0,order+1):
+    var = inputUser()
+    var1 = var[0]
+    var2 = var[1]
+    acoef.append(var1)
+    apower.append(var2)
 
-
-print("YAY")
 #calculation
 
-if order == 2:
-    counter()
-    print("final sumOfAll")
-    print(sumOfAll)
-    print(answer)
+counter()
+print("final sumOfAll")
+print(sumOfAll)
+print(answer)
