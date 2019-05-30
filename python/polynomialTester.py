0e720ce65bca56943d92968addccf0474afc89d2
keaun wild <48817136+Keanu91@users.noreply.github.com>
2019-05-31 09:35:43 +1000

    need to fix variable types

--- python/polynomialTester.py
+++ python/polynomialTester.py
@@ -1,20 +1,31 @@
 #Initiate
 import math
-order = int()
-print("Order of polynomial")
+#variables
+order = int() # Hihest Power
+acoef = [] # array of Coefficients
+apower = [] # array of Powers
+answer = [] # array of answer
+sumOfAll = [] # sumOfAll when calculating
+rangeFrom = float() #lowest x value tested
+rangeTo = float() #Highest x value tested
+rangeStep = float() #Steps of x values
+
+print("Range of X values")
+rangeFrom = float(input("From "))
+rangeTo = float(input("To "))
+print("Stepping Every")
+rangeStep = float(input())
+print("Number of terms")
 order = int(input())
-acoef = []
-apower = []
-answer = []
-sumOfAll = []
+
 
 def inputUser(): # taking in input of user
-    coef = int()
-    power = int()
+    coef = float()
+    power = float()
     print("Enter Coefficient " + str(i))
-    coef = int(input())
+    coef = input()
     print("Enter Power " + str(i))
-    power = int(input(str(coef)+"x^"))
+    power = input(str(coef)+"x^")
     return(coef,power)
 
 def division(coef,x,power):
@@ -23,25 +34,22 @@ def division(coef,x,power):
     return(res)
 
 def counter(): #calculating each section
-    for i2 in range(-100,100): #ramge of x variables tested
-        global x
-        x = i2
-        count = [0]
-        for i3 in range(0,order+1):
-            count.append(division(acoef[i3],x,apower[i3]))
-            print(count)
-        sumOfAll.append(sum(count))
+    xvalue = rangeFrom # X value
+    while xvalue < rangeTo : #range of x variables tested
+        sumVar = [0]
+        for i3 in range(0,order):
+            sumVar.append(division(acoef[i3],xvalue,apower[i3]))
+        sumOfAll.append(sum(sumVar))
         if sumOfAll[-1] != 0:
             sumOfAll.pop()
         else:
-            answer.append(x)
+            answer.append(xvalue)
+        xvalue += rangeStep
 
-for i in range(0,order+1):
+for i in range(0,order):
     var = inputUser()
-    var1 = var[0]
-    var2 = var[1]
-    acoef.append(var1)
-    apower.append(var2)
+    acoef.append(var[0])
+    apower.append(var[1])
 
 #calculation
 
