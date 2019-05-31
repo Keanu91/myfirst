92a77644f11f2c5d9252bf102452c876f819375c
keaun wild <48817136+Keanu91@users.noreply.github.com>
2019-05-31 14:58:00 +1000

    primer done

--- python/polytest/valueTester.py
+++ python/polytest/valueTester.py
@@ -3,19 +3,27 @@ import math
 from fractions import Fraction
 
 def aquireVar(start,stop):
-    listOfVar = []
-    diff = int(stop-start)
     primeNum = []
-    for num in range(lower,upper + 1):   # prime numbers are greater than 1
+    for num in range(start,stop + 1):   # prime numbers are greater than 1
         if num > 1:
             for i in range(2,num):
                 if (num % i) == 0:
                     break
                 else:
                     primeNum.append(num)
-    print(primeNum)
+                if len(primeNum) > 1:
+                    if primeNum[-1] != primeNum[-2]:
+                        break
+                    else:
+                        primeNum.pop()
+    return(primeNum)
 
+def fractionAtor(primes):
+    print(primes)
+    listOfVar = []
+    for i in range(0,len(primes)):
+        print(i)
+        listOfVar.append(Fraction(1,primes[i]))
+    return(listOfVar)
 
-
-
-aquireVar(-10,10)
+#print(fractionAtor(aquireVar(0,100)))
