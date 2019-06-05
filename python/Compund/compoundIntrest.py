ad83a6eb2bb402ffc4211a0e4cd574ed9e0bb023
keaun wild <48817136+Keanu91@users.noreply.github.com>
2019-06-05 15:25:29 +1000

    compound

--- python/Compund/compoundIntrest.py
+++ python/Compund/compoundIntrest.py
@@ -4,4 +4,18 @@ import math
 def userInput():
     print("Input Start Value")
     startVal = int(input())
-    
+    print("Input p.a. rate as percentage eg(5)")
+    rateVal = int(input())
+    print("Input 1 for week based or 2 for monthly based or 3 for yearly based")
+    selectionVal = int(input())
+    for i in range(1,4):
+        if selectionVal == i:
+            print("Input number of unit time to compund eg(2 for compund every 2 months)")
+            deviationVal = int(input())
+            break
+    rate = float()
+    return()
+
+
+
+userInput()
