cd61b6a8875895a38accf9244a3b9fd107984b34
keaun wild <48817136+Keanu91@users.noreply.github.com>
2019-06-17 20:48:04 +1000

    create linux ping version

---
+++ python/networking/ping_linux.py
@@ -0,0 +1,13 @@
+import os
+hostname = "192.168.0."
+result = []
+def ping(ip):
+    response = os.system("ping -c 1 -W 1 " + ip)
+    return(response,ip)
+
+for i in range(0,254): #range of ip
+    response = ping(hostname + str(i))
+    val = response[1]
+    if response[0] == 0:
+        result.append(val)
+print(result)
