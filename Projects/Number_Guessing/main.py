4bcdd60e91a6781d3fc1cb0d8abb0d087db1d22c
keanu <48817136+Keanu91@users.noreply.github.com>
2021-01-21 18:15:58 +1100

    intital guess and some comments

--- Projects/Number_Guessing/main.py
+++ Projects/Number_Guessing/main.py
@@ -4,7 +4,7 @@ def userinput():
     in2 = input("Enter the higher number \n")
     return[in1,in2]
 
-def numcheck(in1):
+def numcheck(in1): #Determines if input is numerical
     for i in range(len(in1)):
         if in1[i].isnumeric() is False:
             return False
@@ -12,13 +12,21 @@ def numcheck(in1):
             i=i+1
     return True
 
+def guessinput(): #function for user guesses
+    guess = input("Please enter your guess\n")
+    return[guess]
+
 print("Welcome to Keaun's Number Guessing Game \n")
-print("To begin please enter a rg")
+print("To begin please enter a range")
 
-rg=userinput()
+rg=userinput() #rg is range
 
-while numcheck(rg) is False:
+while numcheck(rg) is False: #Call function to check input
     print("An entry was non-numeric please try again")
     rg = userinput()
 
-print(rg)
+g1 = guessinput()
+
+while numcheck(g1) is False: #Call function to check input
+    print("An entry was non-numeric please try again")
+    g1 = guessinput()
