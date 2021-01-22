ff0e754f63315c024d05b7574a43711b8acb2118
Keanu91 <48817136+Keanu91@users.noreply.github.com>
2021-01-22 11:26:33 +1100

    finished hangman game

--- Projects/Hangman/main.py
+++ Projects/Hangman/main.py
@@ -1,10 +1,12 @@
 
 def wordcheck(var):
+#    print("wordcheck")
     if var.isalpha() == True:
         return True
     return False
 
 def onechar(var):
+#    print("onechar")
     if len(var) == 1:
         return True
     return False
@@ -15,10 +17,8 @@ def guessletter(guess,spot,word):
     result=[]
     while word.find(guess,spot) != -1:
         spot=word.find(guess,spot)+1
-        result.append(spot)
+        result.append(spot-1)
     return(result)
-
-
 
 print("Welcome to Keaun's Hangman Game")
 print("Please type in the word to be guessed")
@@ -39,18 +39,40 @@ print("All righty it is a "+str(len(word))+" letter word")
 
 finished = 0
 guessnum = 1
+guesses = "*"
+display=[]
 
-while finished = 0:
-    guess=input("Make your "+str(guessnum)" letter guess\n")
+for i in range(len(word)):
+    display.append("")
+print(display)
 
-    while wordcheck(guess) and onechar(guess) != True:
+while finished == 0:
+    guess=input("Make your " + str(guessnum) + " letter guess\n")
+
+    while wordcheck(guess)!=True or onechar(guess)!=True or guessletter(guess,0,guesses)!=False:
         add = " again?" * i
         print("Looks like you didnt follow the rules"+str(add))
         i=i+1
-
-    if guessletter(guess,0,word) is False:
-        print("Unclucky thats a wrong guess\nTry again")
+        guess=input("Make your " + str(guessnum) + " letter guess\n")
         guessnum=guessnum+1
+
+    guesses=guesses+guess
+
+    result=guessletter(guess,0,word)
+
+    if result is False:
+        print("Unclucky thats a wrong guess\nTry again")
     else:
         print("Nice guess")
-        
+        for i in range(len(result)):
+            display[result[i]]=guess
+        print("heres the results")
+        print(display)
+
+    guessnum=guessnum+1
+    var=str("")
+    for i in range(len(display)):
+        var=var+str(display[i])
+    if var == word:
+        finished = 1
+print("congratulations\n the word was\n"+str(word))
