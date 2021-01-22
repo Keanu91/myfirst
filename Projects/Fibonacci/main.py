5ae9d488c49264d183098a1ee6a016923c9c96f5
Keanu91 <48817136+Keanu91@users.noreply.github.com>
2021-01-22 15:18:02 +1100

    fibonacci game update

--- Projects/Fibonacci/main.py
+++ Projects/Fibonacci/main.py
@@ -1,19 +1,16 @@
-
-x = input("put in a number\n")
-length = len(x)
-guess=str("x")
-fibb=[0,1]
-finished = 0
-while finished != 1:
+guess = input("put in a number\n")
+length = len(guess)
+fibseg=str("01")
+fibb=[0,1] #initialise fib sequence
+while str(fibseg) != str(guess):
     fibb.append(fibb[0]+fibb[1])
     var = str(fibb[2])
-    while len(var) > 0:
-        guess = guess + var[0]
+    while len(var) > 0 and str(fibseg) != str(guess):
+        if len(fibseg) == length:
+            fibseg=fibseg[1:]
+        fibseg = fibseg + var[0]
         var = var[1:]
-        print(guess)
-        if str(guess) == str(x):
-            finished = 1
-        if len(guess) == length:
-            guess=guess[1:]
+        print(fibseg)
     del fibb[0]
+
 print(fibb)
